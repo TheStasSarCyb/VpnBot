6741b8eabec642faff2a25ab49538763a47006f9
THEsTAS-cyber <stassariew19@gmail.com>
2025-08-08 19:33:05 +0300

    Added inline button and text/STAS

--- app/handlers/user/start_handlers.py
+++ app/handlers/user/start_handlers.py
@@ -1,6 +1,6 @@
 from aiogram import Router, F
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 from keyboards import keyboards
 from texts import texts
@@ -18,3 +18,7 @@ async def user_wants_to_buy(message: Message):
 @router.message(F.text == texts.MAIN_BUTTON_2)
 async def user_wants_to_buy(message: Message):
     await message.answer(texts.USERS_PROXI)
+
+@router.callback_query(F.data == 'get_prices')
+async def applicate_prices(callback: CallbackQuery):
+    await callback.message.answer(texts.PRICES_OF_PROXY)
--- keyboards/keyboards.py
+++ keyboards/keyboards.py
@@ -1,5 +1,7 @@
 from texts.texts import MAIN_BUTTON_1, MAIN_BUTTON_2
 
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 main = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text=MAIN_BUTTON_1), KeyboardButton(text=MAIN_BUTTON_2)]])
+
+get_prices = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='Узнать цены', callback_data='get_prices')]])
