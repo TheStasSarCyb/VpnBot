eac2e111d0c66909d740e3aaa89905115e6b17af
THEsTAS-cyber <stassariew19@gmail.com>
2025-08-09 10:34:32 +0300

    Added buying keyboard/STAS

--- src/handlers/user/buying_handlers.py
+++ src/handlers/user/buying_handlers.py
@@ -15,3 +15,10 @@ async def applicate_prices(callback: CallbackQuery):
         await callback.answer('Вы выбрали "Купить прокси на 60 дней"')
     if callback.data[-1] == '3':
         await callback.answer('Вы выбрали "Купить прокси на 90 дней"')
+
+@router.message(F.text == texts.MAIN_BUTTON_1)
+async def applicate_prices(message: Message):
+    # await message.answer('Показываю цены')
+    await message.answer(texts.PROXY_VIEW_1, reply_markup=keyboards.buy_proxy_1)
+    await message.answer(texts.PROXY_VIEW_2, reply_markup=keyboards.buy_proxy_2)
+    await message.answer(texts.PROXY_VIEW_3, reply_markup=keyboards.buy_proxy_3)
