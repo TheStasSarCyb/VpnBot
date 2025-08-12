0e574f826494d8edaafe760dce61362d5a42744b
THEsTAS-cyber <stassariew19@gmail.com>
2025-08-12 10:29:50 +0300

    Links to users with confirmations of proxy/STAS

---
+++ APIS/freekassa/__init__.py
@@ -0,0 +1,3 @@
+from APIS.freekassa.frekassa_inits import generate_new_link
+
+__all__ = ['generate_new_link']
--- src/handlers/user/buying_handlers.py
+++ src/handlers/user/buying_handlers.py
@@ -5,6 +5,7 @@ from aiogram.types import Message, CallbackQuery
 from keyboards import keyboards
 from texts import texts
 from APIS.proxy import buying_proxy
+from APIS.freekassa import generate_new_link
 
 router = Router()
 
@@ -14,10 +15,19 @@ async def applicate_prices(callback: CallbackQuery):
     await callback.message.answer(f"Доступных прокси: {count_of_proxy}")
     if callback.data[-1] == '1':
         await callback.answer('Вы выбрали "Купить прокси на 30 дней"')
+        await callback.message.answer(f"{texts.CONFIRMATION_PROXY}\n{texts.PROXY_VIEW_1}")
+        link = generate_new_link(300)
+        await callback.message.answer(f"{texts.PAY_TEXT}\n{link}")
     if callback.data[-1] == '2':
         await callback.answer('Вы выбрали "Купить прокси на 60 дней"')
+        await callback.message.answer(f"{texts.CONFIRMATION_PROXY}\n{texts.PROXY_VIEW_2}")
+        link = generate_new_link(560)
+        await callback.message.answer(f"{texts.PAY_TEXT}\n{link}")
     if callback.data[-1] == '3':
         await callback.answer('Вы выбрали "Купить прокси на 90 дней"')
+        await callback.message.answer(f"{texts.CONFIRMATION_PROXY}\n{texts.PROXY_VIEW_3}")
+        link = generate_new_link(820)
+        await callback.message.answer(f"{texts.PAY_TEXT}\n{link}")
 
 @router.message(F.text == texts.MAIN_BUTTON_1)
 async def applicate_prices(message: Message):
